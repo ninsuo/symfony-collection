b7a1578097392c50e4d5cd4c9f60b1b172bf9c08
alain <ninsuo@gmail.com>
2015-02-23 11:16:00 +0100

    fixes

--- jquery.collection.js
+++ jquery.collection.js
@@ -1,20 +1,3 @@
-/*
- * jquery.collection.js v0.0.2
- *
- * Copyright (c) 2015 alain tiemblo <alain at fuz dot org>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
- * following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 ;(function($) {
 
@@ -146,12 +129,17 @@
             ];
 
             $.each(buttons, function(index, button) {
-               if (settings[button.enabled] && button.condition) {
+               if (settings[button.enabled]) {
                    var action = element.find('.' + button.class);
                    if (action.length === 0 && button.html) {
                        action = $(button.html)
                            .appendTo(actions)
                            .addClass(button.class);
+                   }
+                   if (button.condition) {
+                      action.css('display', 'initial');
+                   } else {
+                      action.css('display', 'none');
                    }
                    action
                      .addClass('collection-action')
