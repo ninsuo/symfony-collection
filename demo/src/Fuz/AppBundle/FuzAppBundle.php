6809b97d8a38cb4e8e59d09032a674db7fa2326f
alain tiemblo <ninsuo@gmail.com>
2016-05-19 21:24:51 +0200

    moved demo/ directory to a separate repository

--- demo/src/Fuz/AppBundle/FuzAppBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fuz\AppBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FuzAppBundle extends Bundle
-{
-}
--- demo/src/Fuz/QuickStartBundle/FuzQuickStartBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Fuz\QuickStartBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FuzQuickStartBundle extends Bundle
-{
-}
